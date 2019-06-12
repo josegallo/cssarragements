3f6bb2f2954c57acb492cc73905e912f6b32565f
josegallo <josegallo@gmail.com>
2019-06-12 15:06:42 +0200

    remove phantom section on specific url

---
+++ elenahuerga.js
@@ -0,0 +1,15 @@
+// // Defining the new alias for jQuery
+// var $j = jQuery.noConflict();
+// $j(document).ready(function(){
+
+// });
+
+    // var pathnameToShow = location.pathname;
+    // console.log(pathnameToShow);
+ 
+   if (location.pathname == "/formulario-sesion-valoracion/") {
+        // console.log ("True");
+    	$j(".title.title_size_large.position_left").css("display", "none");
+    };
+
+
